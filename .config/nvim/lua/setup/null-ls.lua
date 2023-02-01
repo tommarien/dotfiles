424ce94e3ea101fcb21817e021b788e23b466e6c
Tom Marien <tommarien@gmail.com>
2023-02-01 14:09:19 +0100

    config(nvim): remove gitsigns again from null-ls code-actions too much noise

--- .config/nvim/lua/setup/null-ls.lua
+++ .config/nvim/lua/setup/null-ls.lua
@@ -4,7 +4,6 @@ local null_ls = require("null-ls");
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.rustfmt,
     },
