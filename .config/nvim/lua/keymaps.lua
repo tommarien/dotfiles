6d21979790604059a4e58ade971bdf7561e71f69
Tom Marien <tommarien@gmail.com>
2023-02-15 16:37:58 +0100

    config(nvim): exit terminal on esc

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -9,3 +9,5 @@ end
 
 map('n', '-', ':e %:h<CR>', { desc = 'Browse containing folder', silent = true })
 map('n', '<C-s>', ':w<CR>', { desc = 'Save', silent = true })
+
+map('t', '<Esc>', '<C-\\><C-N>', { desc = "Exit Terminal mode", silent = true })
