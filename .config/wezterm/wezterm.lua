8ddac6c43822a1d3f0e48b872ac7fba24b2d151d
Tom Marien <tommarien@gmail.com>
2023-02-26 20:03:59 +0100

    config: setup wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm';
+local rosePine = require 'lua/rose-pine-moon';
+
+return {
+    adjust_window_size_when_changing_font_size = false,
+    colors = rosePine.colors(),
+    hide_tab_bar_if_only_one_tab = true,
+    font = wezterm.font {
+        family = 'MonoLisa',
+         harfbuzz_features = { 'ss02' },
+    },
+    font_size = 15.0,
+    window_frame = rosePine.window_frame(),
+}
