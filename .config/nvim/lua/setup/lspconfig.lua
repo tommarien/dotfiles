60f01e4f595c35cd4f29e80d13e8ef75439a9340
Tom Marien <tommarien@gmail.com>
2023-02-27 16:02:56 +0100

    config(nvim): change keybind for code action to ga, instead of ca

--- .config/nvim/lua/setup/lspconfig.lua
+++ .config/nvim/lua/setup/lspconfig.lua
@@ -34,7 +34,7 @@ local on_attach = function(client, bufnr)
     end, bufopts)
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>ga', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<leader>F', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
