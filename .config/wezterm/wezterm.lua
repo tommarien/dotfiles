ed8279b054016ce9e161afeca941784d849f353a
Tom Marien <tommarien@gmail.com>
2023-02-27 16:03:53 +0100

    config(wezterm): add SUPER + k to clear screen

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,8 +7,15 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     font = wezterm.font {
         family = 'MonoLisa',
-         harfbuzz_features = { 'ss02' },
+        harfbuzz_features = { 'ss02' },
     },
     font_size = 15.0,
     window_frame = rosePine.window_frame(),
+    keys = {
+        {
+            key = 'k',
+            mods = 'SUPER',
+            action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+        },
+    },
 }
