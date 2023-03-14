1fbe92f190b93742ae21b4978592e5a3b6bfae29
Tom Marien <tommarien@gmail.com>
2023-03-14 08:08:28 +0100

    config(nvim): moved lualine config to after

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -150,9 +150,6 @@ return require('packer').startup({
     use {
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-      config = function()
-        require('setup.lualine')
-      end
     }
 
     use {
