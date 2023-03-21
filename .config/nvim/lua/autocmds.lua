59dd8fea2b612618807c04d4cc689bf50e80de9c
Tom Marien <tommarien@gmail.com>
2023-03-21 17:37:41 +0100

    config(nvim): jsonFtDetect autocmds added

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -8,3 +8,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+local jsonFtDetect = vim.api.nvim_create_augroup('jsonFtDetect', { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "tsconfig*.json", ".eslintrc.json" },
+  group = jsonFtDetect,
+  callback = function(ev)
+    vim.api.nvim_buf_call(ev.buf, function()
+      vim.api.nvim_cmd({ cmd = 'setf', args = { 'jsonc' } }, {})
+    end)
+  end,
+})
