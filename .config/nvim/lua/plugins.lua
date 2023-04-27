bd022b9c3963e5779410ae37c84a681e7dabcec8
Tom Marien <tommarien@gmail.com>
2023-04-27 11:53:51 +0200

    config(nvim): setup copilot

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,285 +1,296 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup({
-  function(use)
-    use 'wbthomason/packer.nvim'
+    function(use)
+        use 'wbthomason/packer.nvim'
 
-    use 'tpope/vim-commentary'
-    use({
-      'kylechui/nvim-surround',
-      tag = '*',
-      config = function()
-        require('nvim-surround').setup {}
-      end
-    })
-
-    use {
-      'wellle/targets.vim',
-      tag = '*'
-    }
-
-    -- Git
-    use {
-      'TimUntersberger/neogit',
-      requires = 'nvim-lua/plenary.nvim',
-      config = function()
-        require('neogit').setup({
-          integrations = {
-            diffview = true,
-          }
+        use 'tpope/vim-commentary'
+        use({
+            'kylechui/nvim-surround',
+            tag = '*',
+            config = function()
+                require('nvim-surround').setup {}
+            end
         })
-      end
-    }
 
-    use {
-      'lewis6991/gitsigns.nvim',
-      tag = '*',
-      config = function()
-        require('gitsigns').setup()
-      end
-    }
-    use {
-      'sindrets/diffview.nvim',
-      requires = { { 'nvim-lua/plenary.nvim' }, { 'kyazdani42/nvim-web-devicons' } },
-      config = function()
-        require('diffview').setup({
-          use_icons = true,
-        })
-      end
-    }
-
-    -- Language
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-    }
-
-    use({
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      after = "nvim-treesitter",
-      requires = "nvim-treesitter/nvim-treesitter",
-    })
-
-    use {
-      'jose-elias-alvarez/null-ls.nvim',
-      requires = { { 'nvim-lua/plenary.nvim' } },
-    }
-
-    use {
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-    }
-
-    use({
-      'hrsh7th/nvim-cmp',
-      requires = {
-        'onsails/lspkind.nvim',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'saadparwaiz1/cmp_luasnip',
-        'L3MON4D3/LuaSnip',
-        'rafamadriz/friendly-snippets',
-      },
-    })
-
-    use({
-      'neovim/nvim-lspconfig',
-      after = {
-        'mason-lspconfig.nvim',
-      },
-      requires = {
-        {
-          'simrat39/rust-tools.nvim',
-          'b0o/schemastore.nvim',
-        },
-      },
-    })
-
-    -- Debugging
-    use {
-      'rcarriga/nvim-dap-ui',
-      requires = { 'mfussenegger/nvim-dap' },
-      config = function()
-        local dap, dapui = require("dap"), require("dapui")
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-          dapui.open()
-        end
-        dap.listeners.before.event_terminated["dapui_config"] = function()
-          dapui.close()
-        end
-        dap.listeners.before.event_exited["dapui_config"] = function()
-          dapui.close()
-        end
-      end
-    }
-
-    -- Utilities
-    use { 'stevearc/dressing.nvim' }
-
-    use {
-      'smjonas/live-command.nvim',
-      tag = '*',
-      config = function()
-        require('live-command').setup {
-          commands = {
-            Norm = { cmd = 'norm' },
-          },
+        use {
+            'wellle/targets.vim',
+            tag = '*'
         }
-      end
-    }
 
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    }
-
-    use {
-      'j-hui/fidget.nvim',
-      config = function()
-        require 'fidget'.setup({
-          window = {
-            blend = 0,
-          },
-        })
-      end
-    }
-
-    use({
-      "vuki656/package-info.nvim",
-      requires = "MunifTanjim/nui.nvim",
-      config = function()
-        require("package-info").setup({
-          autostart = false,
-        })
-      end
-    })
-
-    use {
-      'saecki/crates.nvim',
-      tag = '*',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('crates').setup {
+        -- Git
+        use {
+            'TimUntersberger/neogit',
+            requires = 'nvim-lua/plenary.nvim',
+            config = function()
+                require('neogit').setup({
+                    integrations = {
+                        diffview = true,
+                    }
+                })
+            end
         }
-      end,
-    }
 
-    use {
-      'akinsho/toggleterm.nvim',
-      tag = '*',
-    }
+        use {
+            'lewis6991/gitsigns.nvim',
+            tag = '*',
+            config = function()
+                require('gitsigns').setup()
+            end
+        }
+        use {
+            'sindrets/diffview.nvim',
+            requires = { { 'nvim-lua/plenary.nvim' }, { 'kyazdani42/nvim-web-devicons' } },
+            config = function()
+                require('diffview').setup({
+                    use_icons = true,
+                })
+            end
+        }
 
-    use {
-      'folke/which-key.nvim',
-      tag = '*',
-    }
+        -- Language
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = ':TSUpdate',
+        }
 
-    use {
-      'folke/trouble.nvim',
-      tag = '*',
-      requires = 'nvim-tree/nvim-web-devicons',
-    }
-
-    use({
-      'rose-pine/neovim',
-      as = 'rose-pine',
-      config = function()
-        require('rose-pine').setup({
-          dark_variant = 'moon',
-          dim_nc_background = true,
+        use({
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            after = "nvim-treesitter",
+            requires = "nvim-treesitter/nvim-treesitter",
         })
-        vim.cmd('colorscheme rose-pine')
 
-        local p = require("rose-pine.palette");
+        use {
+            'jose-elias-alvarez/null-ls.nvim',
+            requires = { { 'nvim-lua/plenary.nvim' } },
+        }
 
-        vim.api.nvim_set_hl(0, 'PackageInfoUpTodateVersion', { fg = p.pine })
-        vim.api.nvim_set_hl(0, 'PackageInfoOutdatedVersion', { fg = p.love })
-      end
-    })
+        use {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+        }
 
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = {
-        { 'nvim-lua/plenary.nvim' },
-        { 'nvim-telescope/telescope-fzy-native.nvim' },
-        { 'nvim-telescope/telescope-live-grep-args.nvim' },
-        { 'gbrlsnchs/telescope-lsp-handlers.nvim' },
-      },
-      config = function()
-        local telescope = require 'telescope';
-
-        telescope.setup({
-          defaults = {
-            layout_strategy = "horizontal",
-            layout_config = { prompt_position = "top" },
-            sorting_strategy = "ascending",
-            winblend = 0,
-          },
-          pickers = {
-            buffers = {
-              show_all_buffers = true,
-              sort_lastused = true,
-              previewer = false,
-              mappings = {
-                i = { ["<c-d>"] = "delete_buffer" }
-              }
+        use({
+            'hrsh7th/nvim-cmp',
+            requires = {
+                'onsails/lspkind.nvim',
+                'hrsh7th/cmp-nvim-lsp',
+                'hrsh7th/cmp-buffer',
+                'hrsh7th/cmp-path',
+                'hrsh7th/cmp-cmdline',
+                'saadparwaiz1/cmp_luasnip',
+                'L3MON4D3/LuaSnip',
+                'rafamadriz/friendly-snippets',
             },
-            find_files = {
-              previewer = false,
+        })
+
+        use({
+            'neovim/nvim-lspconfig',
+            after = {
+                'mason-lspconfig.nvim',
             },
-          }
-        });
+            requires = {
+                {
+                    'simrat39/rust-tools.nvim',
+                    'b0o/schemastore.nvim',
+                },
+            },
+        })
 
-        telescope.load_extension('live_grep_args');
-        telescope.load_extension('lsp_handlers');
-        telescope.load_extension('fzy_native');
-      end
-    }
-
-    use {
-      'ThePrimeagen/harpoon',
-      requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-
-    use {
-      'folke/zen-mode.nvim',
-      config = function()
-        require('zen-mode').setup {
-          window = {
-            width = 140,
-            options = {
-              number = true,
-              relativenumber = true,
-            }
-          },
+        -- Debugging
+        use {
+            'rcarriga/nvim-dap-ui',
+            requires = { 'mfussenegger/nvim-dap' },
+            config = function()
+                local dap, dapui = require("dap"), require("dapui")
+                dap.listeners.after.event_initialized["dapui_config"] = function()
+                    dapui.open()
+                end
+                dap.listeners.before.event_terminated["dapui_config"] = function()
+                    dapui.close()
+                end
+                dap.listeners.before.event_exited["dapui_config"] = function()
+                    dapui.close()
+                end
+            end
         }
-      end
-    }
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-      require('packer').sync()
-    end
-  end,
-  config = {
-    display = {
-      open_fn = function()
-        return require('packer.util').float({ border = 'single' })
-      end
+        -- Utilities
+        use { 'stevearc/dressing.nvim' }
+
+        use {
+            'smjonas/live-command.nvim',
+            tag = '*',
+            config = function()
+                require('live-command').setup {
+                    commands = {
+                        Norm = { cmd = 'norm' },
+                    },
+                }
+            end
+        }
+
+        use {
+            'nvim-lualine/lualine.nvim',
+            requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        }
+
+        use {
+            'j-hui/fidget.nvim',
+            config = function()
+                require 'fidget'.setup({
+                    window = {
+                        blend = 0,
+                    },
+                })
+            end
+        }
+
+        use({
+            "vuki656/package-info.nvim",
+            requires = "MunifTanjim/nui.nvim",
+            config = function()
+                require("package-info").setup({
+                    autostart = false,
+                })
+            end
+        })
+
+        use {
+            'saecki/crates.nvim',
+            tag = '*',
+            requires = { 'nvim-lua/plenary.nvim' },
+            config = function()
+                require('crates').setup {
+                }
+            end,
+        }
+
+        use {
+            'akinsho/toggleterm.nvim',
+            tag = '*',
+        }
+
+        use {
+            'folke/which-key.nvim',
+            tag = '*',
+        }
+
+        use {
+            'folke/trouble.nvim',
+            tag = '*',
+            requires = 'nvim-tree/nvim-web-devicons',
+        }
+
+        use({
+            'rose-pine/neovim',
+            as = 'rose-pine',
+            config = function()
+                require('rose-pine').setup({
+                    dark_variant = 'moon',
+                    dim_nc_background = true,
+                })
+                vim.cmd('colorscheme rose-pine')
+
+                local p = require("rose-pine.palette");
+
+                vim.api.nvim_set_hl(0, 'PackageInfoUpTodateVersion', { fg = p.pine })
+                vim.api.nvim_set_hl(0, 'PackageInfoOutdatedVersion', { fg = p.love })
+            end
+        })
+
+        use {
+            'nvim-telescope/telescope.nvim',
+            requires = {
+                { 'nvim-lua/plenary.nvim' },
+                { 'nvim-telescope/telescope-fzy-native.nvim' },
+                { 'nvim-telescope/telescope-live-grep-args.nvim' },
+                { 'gbrlsnchs/telescope-lsp-handlers.nvim' },
+            },
+            config = function()
+                local telescope = require 'telescope';
+
+                telescope.setup({
+                    defaults = {
+                        layout_strategy = "horizontal",
+                        layout_config = { prompt_position = "top" },
+                        sorting_strategy = "ascending",
+                        winblend = 0,
+                    },
+                    pickers = {
+                        buffers = {
+                            show_all_buffers = true,
+                            sort_lastused = true,
+                            previewer = false,
+                            mappings = {
+                                i = { ["<c-d>"] = "delete_buffer" }
+                            }
+                        },
+                        find_files = {
+                            previewer = false,
+                        },
+                    }
+                });
+
+                telescope.load_extension('live_grep_args');
+                telescope.load_extension('lsp_handlers');
+                telescope.load_extension('fzy_native');
+            end
+        }
+
+        use {
+            'ThePrimeagen/harpoon',
+            requires = { { 'nvim-lua/plenary.nvim' } }
+        }
+
+        use {
+            'folke/zen-mode.nvim',
+            config = function()
+                require('zen-mode').setup {
+                    window = {
+                        width = 140,
+                        options = {
+                            number = true,
+                            relativenumber = true,
+                        }
+                    },
+                }
+            end
+        }
+
+
+        use {
+            'github/copilot.vim',
+            config = function()
+                vim.g.copilot_no_tab_map = true
+                vim.g.copilot_assume_mapped = true
+                vim.keymap.set('i', '<M-j>', 'copilot#Accept("<CR>")',
+                { noremap = true, silent = true, expr = true, replace_keycodes = false })
+            end
+        }
+
+        -- Automatically set up your configuration after cloning packer.nvim
+        -- Put this at the end after all plugins
+        if packer_bootstrap then
+            require('packer').sync()
+        end
+    end,
+    config = {
+        display = {
+            open_fn = function()
+                return require('packer.util').float({ border = 'single' })
+            end
+        }
     }
-  }
 })
