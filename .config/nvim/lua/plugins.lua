3038284a9dc1578862d8b6ed550e1c25e85c5726
Tom Marien <tommarien@gmail.com>
2023-06-24 10:19:13 +0200

    config(nvim): move to christoomey/vim-tmux-navigator

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -155,20 +155,7 @@ return require('packer').startup({
         use 'mbbill/undotree'
 
         use {
-            'alexghergh/nvim-tmux-navigation',
-            config = function()
-                require 'nvim-tmux-navigation'.setup {
-                    disable_when_zoomed = true, -- defaults to false
-                    keybindings = {
-                        left = '<C-h>',
-                        down = '<C-j>',
-                        up = '<C-k>',
-                        right = '<C-l>',
-                        last_active = '<C-\\>',
-                        next = '<C-Space>',
-                    }
-                }
-            end
+            'christoomey/vim-tmux-navigator',
         }
 
         use {
