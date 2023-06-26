afe36d11448cbe4cd7272a6f80b39a3fcff27de8
Tom Marien <tommarien@gmail.com>
2023-06-26 19:58:42 +0200

    config(nvim): try to see if word counter is culprit for hangs

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -5,11 +5,11 @@ local luasnip = require 'luasnip'
 local lspkind = require 'lspkind'
 luasnip.config.setup {}
 
-local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+-- local has_words_before = function()
+--     unpack = unpack or table.unpack
+--     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
 
 cmp.setup({
     formatting = {
@@ -55,8 +55,8 @@ cmp.setup({
                 cmp.select_next_item()
             elseif luasnip.expand_or_locally_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
+            -- elseif has_words_before() then
+            --     cmp.complete()
             else
                 fallback()
             end
