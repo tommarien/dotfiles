0f8ccb1d5fbc6e3878fb8f1ab044bc7dfee763d2
Tom Marien <tommarien@gmail.com>
2023-06-27 20:56:20 +0200

    config(nvim): remove gruvbox completely

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -213,47 +213,6 @@ return require('packer').startup({
         }
 
         use {
-            'ellisonleao/gruvbox.nvim',
-            config = function()
-                local contrast = '' -- can be "hard", "soft" or empty string
-
-                require('gruvbox').setup({
-                    undercurl = true,
-                    underline = true,
-                    bold = true,
-                    italic = {
-                        strings = true,
-                        comments = true,
-                        operators = false,
-                        folds = true,
-                    },
-                    strikethrough = true,
-                    invert_selection = false,
-                    invert_signs = false,
-                    invert_tabline = false,
-                    invert_intend_guides = false,
-                    inverse = true, -- invert background for search, diffs, statuslines and errors
-                    contrast = contrast,
-                    palette_overrides = {},
-                    overrides = {
-                        SignColumn = { link = "Normal" },
-                        GruvboxGreenSign = { bg = "" },
-                        GruvboxOrangeSign = { bg = "" },
-                        GruvboxPurpleSign = { bg = "" },
-                        GruvboxYellowSign = { bg = "" },
-                        GruvboxRedSign = { bg = "" },
-                        GruvboxBlueSign = { bg = "" },
-                        GruvboxAquaSign = { bg = "" },
-                    },
-                    dim_inactive = false,
-                    transparent_mode = false,
-                })
-
-                -- vim.cmd('colorscheme gruvbox')
-            end
-        }
-
-        use {
             'nvim-telescope/telescope.nvim',
             requires = {
                 { 'nvim-lua/plenary.nvim' },
