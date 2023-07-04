bb554464c4162de0b32f715533e5b9e3dfec6d52
Tom Marien <tommarien@gmail.com>
2023-07-04 08:17:30 +0200

    config(nvim): bye bye oil again

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -154,14 +154,6 @@ return require('packer').startup({
             end
         }
 
-        use {
-            'stevearc/oil.nvim',
-            config = function()
-                require('oil').setup()
-                vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
-            end
-        }
-
         use 'mbbill/undotree'
 
         use {
