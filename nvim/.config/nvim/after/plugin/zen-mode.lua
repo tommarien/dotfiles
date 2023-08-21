4a6840a0802b62dd6e237583367e78a41b612a72
Tom Marien <tommarien@gmail.com>
2023-08-21 11:52:53 +0200

    nvim(zen-mode): make the screen abit wider

---
+++ nvim/.config/nvim/after/plugin/zen-mode.lua
@@ -0,0 +1,11 @@
+require("zen-mode").setup {
+    window = {
+        width = 160,
+    },
+    plugins = {
+        kitty = {
+            enabled = false,
+            font = "+2", -- font size increment
+        },
+    }
+}
