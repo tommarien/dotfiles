2d76632c04297199774099ec45d25d8b7a65a124
Tom Marien <tommarien@gmail.com>
2023-10-07 01:11:44 +0200

    wezterm: fine tune abit

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,18 +3,27 @@ local wezterm = require 'wezterm'
 return {
     adjust_window_size_when_changing_font_size = false,
     color_scheme = 'duskfox',
-    font = wezterm.font {
-        family = 'MonoLisa',
-        harfbuzz_features = { 'ss02' }
+    font = wezterm.font_with_fallback {
+        {
+            family = 'MonoLisa',
+            harfbuzz_features = { 'ss02' }
+        },
+        {
+            family = 'Symbols Nerd Font Mono',
+            scale = 0.75
+        }
     },
-    font_size = 15.0,
+    font_size = 16,
     hide_tab_bar_if_only_one_tab = true,
-    window_background_opacity = 0.75,
     macos_window_background_blur = 10,
+    scrollback_lines = 5000,
+    use_cap_height_to_scale_fallback_fonts = true,
+    window_background_opacity = 0.75,
+    window_decorations = "RESIZE", -- Hide titlebar
     window_padding = {
-        left = '0.5cell',
-        right = '0.5cell',
-        top = 0,
+        left = 20,
+        right = 5,
+        top = 10,
         bottom = 0
     }
 }
