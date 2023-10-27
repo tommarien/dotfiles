9dd2cf51afbac6772a9c0a86a765d48dfcd57548
Tom Marien <tommarien@gmail.com>
2023-10-28 00:12:14 +0200

    nvim: cleanup cmp mappings

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -57,27 +57,27 @@ return {
                         luasnip.lsp_expand(args.body)
                     end,
                 },
-                mapping = {
-                    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-                    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-                    ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-                    ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-                    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-                    ["<C-y>"] = cmp.mapping.confirm { select = true },
-                    ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-                    ["<CR>"] = cmp.mapping.confirm { select = false },
-                    ["<Tab>"] = cmp.mapping(function(fallback)
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        local col = vim.fn.col('.') - 1
+
                         if cmp.visible() then
                             cmp.select_next_item()
                         elseif luasnip.expand_or_locally_jumpable() then
                             luasnip.expand_or_jump()
-                        else
+                        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
                             fallback()
+                        else
+                            cmp.complete()
                         end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
                         elseif luasnip.jumpable(-1) then
@@ -85,8 +85,8 @@ return {
                         else
                             fallback()
                         end
-                    end, { "i", "s" }),
-                },
+                    end, { 'i', 's' })
+                }),
                 -- Installed sources
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp', keyword_lenght = 2, max_item_count = 20 },
