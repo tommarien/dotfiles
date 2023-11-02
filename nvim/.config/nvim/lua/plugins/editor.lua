9f65086a7633defeb63564f110033b49029d8092
Tom Marien <tommarien@gmail.com>
2023-11-02 11:37:10 +0100

    nvim: added Aerial

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -162,6 +162,23 @@ return {
         },
     },
     {
+        'stevearc/aerial.nvim',
+        event = 'VeryLazy',
+        keys = {
+            { '<leader>a', vim.cmd.AerialToggle, desc = 'AerialToggle' },
+        },
+        opts = {
+            on_attach = function(bufnr)
+                vim.keymap.set("n", "[a", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                vim.keymap.set("n", "]a", "<cmd>AerialNext<CR>", { buffer = bufnr })
+            end,
+        },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+    },
+    {
         'tpope/vim-abolish',
         event = 'VeryLazy',
     },
