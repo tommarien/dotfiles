12427a62eefbe2e44e11b8a3ed95851ba11524e1
Tom Marien <tommarien@gmail.com>
2023-11-02 08:22:33 +0100

    Try a light colorschema

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -5,7 +5,7 @@ return {
         priority = 1000,
         opts = {
             options = {
-                transparent = true,
+                -- transparent = true,
                 styles = {               -- Style to be applied to different syntax groups
                     comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
                     conditionals = 'NONE',
@@ -23,16 +23,29 @@ return {
                 all = {
                     EyelinerPrimary = { fg = 'palette.pink.bright', bg = 'NONE' },
                     EyelinerSecondary = { fg = 'palette.cyan.bright', bg = 'NONE' },
-                },
-                duskfox = {
                     NormalFloat = { bg = 'NONE' },
-                }
+
+                },
             }
         },
         config = function(_, opts)
             require('nightfox').setup(opts)
 
-            vim.cmd('colorscheme duskfox')
+            vim.cmd('colorscheme dawnfox')
         end
     },
+    {
+        'sainnhe/edge',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.o.background = 'light'
+            vim.g.edge_better_performance = 1
+
+            -- vim.cmd('colorscheme edge')
+
+            vim.api.nvim_set_hl(0, 'EyelinerPrimary', { link = 'QuickScopePrimary' })
+            vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg = '#5079be' })
+        end
+    }
 }
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,26 +3,8 @@ local wezterm = require 'wezterm'
 return {
     adjust_window_size_when_changing_font_size = false,
     bold_brightens_ansi_colors = 'No',
-    color_scheme = 'duskfox',
-    -- Kanagawa
-    -- colors = {
-    --     foreground = "#dcd7ba",
-    --     background = "#1f1f28",
-    --
-    --     cursor_bg = "#c8c093",
-    --     cursor_fg = "#c8c093",
-    --     cursor_border = "#c8c093",
-    --
-    --     selection_fg = "#c8c093",
-    --     selection_bg = "#2d4f67",
-    --
-    --     scrollbar_thumb = "#16161d",
-    --     split = "#16161d",
-    --
-    --     ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-    --     brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-    --     indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-    -- },
+    -- color_scheme = 'Edge Light (base16)',
+    color_scheme = 'dawnfox',
     font = wezterm.font_with_fallback {
         -- {
         --     family = 'JetBrains Mono',
@@ -43,11 +25,11 @@ return {
     font_size = 16,
     hide_tab_bar_if_only_one_tab = true,
     line_height = 1.02,
-    macos_window_background_blur = 20,
+    -- macos_window_background_blur = 20,
     scrollback_lines = 5000,
     term = 'wezterm',
     use_cap_height_to_scale_fallback_fonts = true,
-    window_background_opacity = 0.9,
+    -- window_background_opacity = 0.9,
     window_decorations = 'RESIZE', -- Hide titlebar
     window_padding = {
         left = 20,
