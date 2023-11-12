5dbfea7ef1f3f1d6a7afe3280d77ce8f6a20a8c5
Tom Marien <tommarien@gmail.com>
2023-11-12 18:06:15 +0100

    nvim: revert back to confirm

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -25,91 +25,8 @@ return {
             'williamboman/mason-lspconfig.nvim',
             'simrat39/rust-tools.nvim',
             'b0o/schemastore.nvim',
-            'mattn/efm-langserver'
         },
         config = function()
-                        -- Switch for controlling whether you want autoformatting.
-            --  Use :FormatOnSaveToggle to toggle autoformatting on or off
-            local format_on_save_enabled = true
-
-            vim.api.nvim_create_user_command('FormatOnSaveToggle', function()
-                format_on_save_enabled = not format_on_save_enabled
-                if format_on_save_enabled then
-                    vim.notify("Enabled format on save", vim.log.levels.INFO)
-                else
-                    vim.notify("Disabled format on save", vim.log.levels.INFO)
-                end
-            end, {})
-
-            -- Gets all lsp clients that support formatting
-            -- and have not disabled it in their client config
-            -- @param client lsp.Client
-            local supports_format = function(client)
-                if
-                    client.config
-                    and client.config.capabilities
-                    and client.config.capabilities.documentFormattingProvider == false
-                then
-                    return false
-                end
-                return client.supports_method("textDocument/formatting") or
-                    client.supports_method("textDocument/rangeFormatting")
-            end
-
-            -- Format current buffer
-            -- @param opts? { bufnr?:number, async?:boolean, silent?:boolean }
-            local format = function(opts)
-                opts = opts or {}
-                local bufnr = opts.bufnr or vim.api.nvim_get_current_buf()
-
-                local active_clients = vim.lsp.get_active_clients({ bufnr })
-                local formatting_clients = vim.tbl_filter(supports_format, active_clients)
-
-                local efm = nil
-
-                for _, cl in ipairs(formatting_clients) do
-                    if cl.name == "efm" then
-                        efm = cl
-                        break
-                    end
-                end
-
-                local silent = opts.silent or false
-
-                if vim.tbl_isempty(formatting_clients) then
-                    if not silent then
-                        vim.notify("No lsp clients attached that support formatting", vim.log.levels.WARN)
-                    end
-                    return
-                end
-
-                local async = opts.async or true
-
-                vim.lsp.buf.format({
-                    async,
-                    filter = function(client)
-                        if not efm then
-                            return true
-                        else
-                            return client.id == efm.id
-                        end
-                    end,
-                })
-            end
-
-            vim.api.nvim_create_user_command('Format', function()
-                format()
-            end, { desc = "Format current buffer with LSP" })
-
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = vim.api.nvim_create_augroup("FormatOnSave", {}),
-                callback = function()
-                    if format_on_save_enabled then
-                        format({ async = false, silent = true })
-                    end
-                end,
-            })
-
             -- Use an on_attach function to only map the following keys
             -- after the language server attaches to the current buffer
             local on_attach = function(_, bufnr)
@@ -135,7 +52,6 @@ return {
                 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
                 vim.keymap.set('n', '<M-.>', vim.lsp.buf.code_action, bufopts)
                 vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, bufopts)
-                vim.keymap.set({ 'n', 'v' }, '<leader>F', function() format({ async = true, bufnr }) end, bufopts)
             end
 
             -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
@@ -242,59 +158,6 @@ return {
             };
 
             rt.setup(rust_opts);
-
-            -- efm
-            local prettier = {
-                formatCanRange = true,
-                formatCommand =
-                [[prettierd --stdin-filepath ${INPUT} ${--range-start=charStart} ${--range-end=charEnd} ${--tab-width:tab_width} ${--use-tabs=!insertSpaces}]],
-                formatStdin = true,
-                rootMarkers = {
-                    ".prettierrc",
-                    ".prettierrc.json",
-                    ".prettierrc.js",
-                    ".prettierrc.yml",
-                    ".prettierrc.yaml",
-                    ".prettierrc.json5",
-                    ".prettierrc.mjs",
-                    ".prettierrc.cjs",
-                    ".prettierrc.toml",
-                },
-            }
-
-            local prettier_formatted_languages = {
-                "css",
-                "html",
-                "javascript",
-                "javascript.jsx",
-                "javascriptreact",
-                "json",
-                "jsonc",
-                "markdown",
-                "scss",
-                "typescript",
-                "typescript.tsx",
-                "typescriptreact",
-                "yaml",
-            }
-
-            local efm_languages = {}
-
-            for _, lang in pairs(prettier_formatted_languages) do
-                efm_languages[lang] = { prettier }
-            end
-
-            nvim_lsp.efm.setup {
-                filetypes = prettier_formatted_languages,
-                on_attach = on_attach,
-                init_options = {
-                    documentFormatting = true,
-                    documentRangeFormatting = true,
-                },
-                settings = {
-                    languages = efm_languages
-                },
-            }
         end
     },
 }
