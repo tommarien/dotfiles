298c2446d6dc733cc681b9b3d915eab341ea47ca
Tom Marien <tommarien@gmail.com>
2023-11-24 17:35:55 +0100

    nvim: create CopilotToggle command

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -3,6 +3,19 @@ return {
         'zbirenbaum/copilot.lua',
         cmd = 'Copilot',
         event = 'InsertEnter',
+        init = function()
+            local copilot_on = true
+            vim.api.nvim_create_user_command('CopilotToggle', function()
+                if copilot_on then
+                    vim.cmd("Copilot disable")
+                    print("Copilot OFF")
+                else
+                    vim.cmd("Copilot enable")
+                    print("Copilot ON")
+                end
+                copilot_on = not copilot_on
+            end, { nargs = 0 })
+        end,
         opts = {
             suggestion = {
                 enabled = true,
