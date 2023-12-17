6e282db05bd390da573c7a25dee9123bf5b7c4ae
Tom Marien <tommarien@gmail.com>
2023-12-17 01:08:12 +0100

    nvim: add gruvbox.nvim and kanagawa.nvim

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -17,8 +17,35 @@ return {
             }
         },
         config = function(_, opts)
-            require('ayu').setup(opts)
-            vim.cmd('colorscheme ayu')
+            -- require('ayu').setup(opts)
+            -- vim.cmd('colorscheme ayu')
+        end
+    },
+    {
+        'rebelot/kanagawa.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true,
+        },
+        config = function(_, opts)
+            require('kanagawa').setup(opts)
+            vim.cmd('colorscheme kanagawa')
+        end
+    },
+    {
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
+        opts = {
+            transparent_mode = true,
+            contrast = 'hard',
+            italic = {
+                strings = false
+            }
+        },
+        config = function(_, opts)
+            require('gruvbox').setup(opts)
+            -- vim.cmd('colorscheme gruvbox')
         end
     }
 }
