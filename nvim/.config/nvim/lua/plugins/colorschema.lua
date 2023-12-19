0a65e84659833986f36050f25f68705fc4141d9d
Tom Marien <tommarien@gmail.com>
2023-12-20 00:47:09 +0100

    chore: clean up

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -34,23 +34,6 @@ return {
         end
     },
     {
-        'neanias/everforest-nvim',
-        version = false,
-        lazy = false,
-        priority = 1000,
-        opts = {
-            on_highlights = function(hl, palette)
-                hl.EyelinerPrimary = { bg = palette.green, fg = palette.bg1 }
-                hl.EyelinerSecondary = { bg = palette.orange, fg = palette.bg1 }
-            end
-        },
-        config = function(_, opts)
-            -- vim.cmd('set background=light')
-            -- require("everforest").setup(opts)
-            -- vim.cmd('colorscheme everforest')
-        end,
-    },
-    {
         'sainnhe/sonokai',
         lazy = false,
         priority = 1000,
@@ -59,6 +42,7 @@ return {
             vim.g.sonokai_diagnostic_virtual_text = 'highlighted'
             vim.g.sonokai_transparent_background = 1
             vim.g.sonokai_better_performance = 1
+            vim.g.sonokai_enable_italic = 1
             vim.cmd('set background=dark')
             vim.cmd('colorscheme sonokai')
         end
