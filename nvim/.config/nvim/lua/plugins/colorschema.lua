9855ccad6149c01fbb8ce361f847e20d44638526
Tom Marien <tommarien@gmail.com>
2024-01-05 21:30:25 +0100

    nvim: use everforest as light

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -8,21 +8,20 @@ return {
             vim.g.sonokai_better_performance = 1
             vim.g.sonokai_transparent_background = 1
             vim.g.sonokai_diagnostic_virtual_text = 'colored'
-            -- vim.g.sonokai_enable_italic = 1
             vim.cmd('colorscheme sonokai')
         end
     },
     {
-        'yorik1984/newpaper.nvim',
+        'sainnhe/everforest',
         lazy = false,
-        priority = 1001,
-        opts = {
-            italic_strings = false,
-            booleans = 'NONE',
-            keywords = 'NONE'
-        },
-        config = function(_, opts)
-            require('newpaper').setup(opts)
+        priority = 1000,
+        config = function()
+            vim.g.everforest_better_performance = 1
+            vim.g.everforest_diagnostic_virtual_text = 'colored'
+            vim.g.everforest_enable_bold = 1
+            -- vim.g.everforest_transparent_background = 1
+            -- We set the background to light because i only use the light
+            vim.opt.background = 'light'
         end
     }
 }
