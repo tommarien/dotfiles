a86884ce0866467b5e2ecdc1cf353790c873a9d0
Tom Marien <tommarien@gmail.com>
2024-01-10 23:11:18 +0100

    nvim: added leetcode

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,3 +1,5 @@
+local leet_arg = "leetcode.nvim"
+
 return {
     {
         'JoosepAlviste/nvim-ts-context-commentstring',
@@ -21,6 +23,24 @@ return {
         event = 'VeryLazy',
         version = '*',
         opts = {},
+    },
+    {
+        'kawre/leetcode.nvim',
+        build = ":TSUpdate html",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim", -- required by telescope
+            "MunifTanjim/nui.nvim",
+
+            -- optional
+            "nvim-treesitter/nvim-treesitter",
+            "rcarriga/nvim-notify",
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = leet_arg ~= vim.fn.argv()[1],
+        opts = {
+            arg = leet_arg,
+        },
     },
     {
         'windwp/nvim-autopairs',
