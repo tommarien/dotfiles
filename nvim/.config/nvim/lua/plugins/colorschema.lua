6a99c3d5dcd24329219d359d2a91fd8a98a7951d
Tom Marien <tommarien@gmail.com>
2024-01-13 21:48:46 +0100

    nvim: remove sonokai

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -1,21 +1,5 @@
 return {
     {
-        'sainnhe/sonokai',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.sonokai_style = 'atlantis'
-            vim.g.sonokai_better_performance = 1
-            vim.g.sonokai_transparent_background = 2
-            vim.g.sonokai_diagnostic_virtual_text = 'colored'
-            -- vim.cmd('colorscheme sonokai')
-
-            -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
-            -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE' })
-            -- vim.api.nvim_set_hl(0, 'MatchParen', { bg = 'NONE', fg = '#87ff00', bold = true })
-        end
-    },
-    {
         'sainnhe/edge',
         lazy = false,
         priority = 1000,
