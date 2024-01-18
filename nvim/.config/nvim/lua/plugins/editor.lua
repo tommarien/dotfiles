8d27e70631c9c7d96b42bc63ff61a373d0efc045
Tom Marien <tommarien@gmail.com>
2024-01-18 06:56:58 +0100

    nvim: remove leetcode

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -25,24 +25,6 @@ return {
         opts = {},
     },
     {
-        'kawre/leetcode.nvim',
-        build = ":TSUpdate html",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim", -- required by telescope
-            "MunifTanjim/nui.nvim",
-
-            -- optional
-            "nvim-treesitter/nvim-treesitter",
-            "rcarriga/nvim-notify",
-            "nvim-tree/nvim-web-devicons",
-        },
-        lazy = leet_arg ~= vim.fn.argv()[1],
-        opts = {
-            arg = leet_arg,
-        },
-    },
-    {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         opts = {}
