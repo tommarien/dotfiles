fc76f93a53154ad286aaf9f2c1772bbbd03be3b3
Tom Marien <tommarien@gmail.com>
2024-02-26 20:54:33 +0100

    nvim: cleanup colorschema.lua

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -1,50 +1,5 @@
 return {
     {
-        'sainnhe/edge',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.edge_style = 'aura'
-            vim.g.edge_better_performance = 1
-            vim.g.edge_diagnostic_virtual_text = 'colored'
-            vim.g.edge_enable_bold = 1
-            vim.g.edge_transparent_background = 0
-
-            -- vim.cmd('colorscheme edge')
-        end
-    },
-    {
-        'folke/tokyonight.nvim',
-        lazy = false,
-        priority = 1000,
-        opts = {
-            style = 'storm',
-            styles = {
-                floats = 'normal',
-                keywords = { italic = false },
-            },
-            lualine_bold = true,
-        },
-        config = function(_, opts)
-            require('tokyonight').setup(opts)
-            -- vim.cmd('colorscheme tokyonight')
-        end
-    },
-    {
-        'ellisonleao/gruvbox.nvim',
-        lazy = false,
-        priority = 1000,
-        opts = {
-            overrides = {
-                SignColumn = { link = 'LineNr' },
-            }
-        },
-        config = function(_, opts)
-            require('gruvbox').setup(opts)
-            -- vim.cmd('colorscheme gruvbox')
-        end
-    },
-    {
         'sainnhe/everforest',
         lazy = false,
         priority = 1000,
