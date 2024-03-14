eb75002866b9f967426e62394a25baa3b6c63bfd
Tom Marien <tommarien@gmail.com>
2024-03-14 18:08:30 +0100

    theme: changed to gruvbox-dark-soft

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -6,7 +6,19 @@ return {
         config = function()
             vim.g.everforest_better_performance = 1
             vim.g.everforest_diagnostic_virtual_text = 1
-            vim.cmd('colorscheme everforest')
+            -- vim.cmd('colorscheme everforest')
         end
-    }
+    },
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.gruvbox_material_foreground = 'original'
+            vim.g.gruvbox_material_background = 'soft'
+            vim.g.gruvbox_material_better_performance = 1
+            vim.g.gruvbox_material_diagnostic_virtual_text = 1
+            vim.cmd('colorscheme gruvbox-material')
+        end
+    },
 }
