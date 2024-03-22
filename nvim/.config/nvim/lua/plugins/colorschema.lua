5249b75411b14a0e5e8b67af3dcd811e8e99421f
Tom Marien <tommarien@gmail.com>
2024-03-22 16:51:33 +0100

    nvim: cleanup colorscheme

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -1,15 +1,5 @@
 return {
     {
-        'sainnhe/everforest',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.everforest_better_performance = 1
-            vim.g.everforest_diagnostic_virtual_text = 1
-            -- vim.cmd('colorscheme everforest')
-        end
-    },
-    {
         'sainnhe/gruvbox-material',
         lazy = false,
         priority = 1000,
