7a2173fda63d7fefec91acd06c96793e90044f5d
Tom Marien <tommarien@gmail.com>
2024-03-22 16:50:45 +0100

    nvim: fix whichkeysep

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -19,6 +19,11 @@ return {
             vim.g.gruvbox_material_better_performance = 1
             vim.g.gruvbox_material_diagnostic_virtual_text = 1
             vim.cmd('colorscheme gruvbox-material')
+
+            -- Override WhichKeySeparator, otherwise would be displayed as italic
+            local hl_cmt = vim.api.nvim_get_hl_by_name("Comment", true)
+            local hl_sep = vim.tbl_extend("force", hl_cmt, { italic = false })
+            return vim.api.nvim_set_hl(0, "WhichKeySeparator", hl_sep)
         end
     },
 }
