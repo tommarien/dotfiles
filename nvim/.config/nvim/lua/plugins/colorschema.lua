23a67ef77f4bddbadacbc39fb4c23ca03cf30bff
Tom Marien <tommarien@gmail.com>
2024-04-03 08:08:00 +0200

    nvim: updated plugins

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -5,7 +5,7 @@ return {
         priority = 1000,
         config = function()
             vim.g.gruvbox_material_foreground = 'original'
-            -- vim.g.gruvbox_material_background = 'soft'
+            -- vim.g.gruvbox_material_background = 'hard'
             vim.g.gruvbox_material_better_performance = 1
             vim.g.gruvbox_material_diagnostic_virtual_text = 1
             vim.cmd('colorscheme gruvbox-material')
