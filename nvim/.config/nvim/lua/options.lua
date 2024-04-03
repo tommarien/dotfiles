d95a6c86319365e4fba577c49a4caa6f523c423a
Tom Marien <tommarien@gmail.com>
2024-04-03 08:07:43 +0200

    nvim(options): enable spelling

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -32,6 +32,9 @@ o.shortmess = o.shortmess .. "c"
 o.hlsearch = false -- Never highlight all search matches
 o.incsearch = true -- Search for strings incrementally
 
+-- Spelling
+o.spell = true
+
 -- Makes neovim and host OS clipboard play nicely with each other
 o.clipboard = 'unnamedplus'
 
