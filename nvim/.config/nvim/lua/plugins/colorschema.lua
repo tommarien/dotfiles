32cd78d56d38c547107948a5a56e0eb81bfa3547
Tom Marien <tommarien@gmail.com>
2024-04-09 23:26:45 +0200

    nvim(colorschema): tweaked colors abit

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -10,12 +10,16 @@ return {
             vim.g.gruvbox_material_diagnostic_virtual_text = 1
             -- vim.g.gruvbox_material_transparent_background = 1
             vim.g.gruvbox_material_enable_bold = 1
+            vim.g.gruvbox_material_colors_override = {
+                red = { '#f2594b', 167 },
+            }
+
             vim.cmd('colorscheme gruvbox-material')
 
             -- Override WhichKeySeparator, otherwise would be displayed as italic
             local hl_cmt = vim.api.nvim_get_hl_by_name("Comment", true)
             local hl_sep = vim.tbl_extend("force", hl_cmt, { italic = false })
-            return vim.api.nvim_set_hl(0, "WhichKeySeparator", hl_sep)
+            vim.api.nvim_set_hl(0, "WhichKeySeparator", hl_sep)
         end
-    },
+    }
 }
