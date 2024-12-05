6d5cba6edbf02f6a49117c1961558e1c4a0cabb2
Tom Marien <tommarien@gmail.com>
2024-12-05 23:54:25 +0100

    nvim: added maximize test window

--- nvim/.config/nvim/lua/plugins/testing.lua
+++ nvim/.config/nvim/lua/plugins/testing.lua
@@ -2,11 +2,15 @@ return {
     {
         'vim-test/vim-test',
         event = 'VeryLazy',
+        dependencies = {
+            'preservim/vimux'
+        },
         keys = {
-            { "<leader>tt", vim.cmd.TestNearest, desc = 'Run Nearest Test' },
-            { "<leader>tf", vim.cmd.TestFile,    desc = 'Run Test File' },
-            { "<leader>ts", vim.cmd.TestSuite,   desc = 'Run All Test Files' },
-            { "<leader>tr", vim.cmd.TestLast,    desc = 'Run Last Test Run' }
+            { "<leader>tt", vim.cmd.TestNearest,     desc = 'Run Nearest Test' },
+            { "<leader>tf", vim.cmd.TestFile,        desc = 'Run Test File' },
+            { "<leader>ts", vim.cmd.TestSuite,       desc = 'Run All Test Files' },
+            { "<leader>tr", vim.cmd.TestLast,        desc = 'Run Last Test Run' },
+            { "<leader>tm", vim.cmd.VimuxZoomRunner, desc = 'Maximize Test Runner' }
 
         },
         config = function()
