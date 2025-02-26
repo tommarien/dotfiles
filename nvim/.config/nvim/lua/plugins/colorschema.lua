6361122a1460d09772e4353da12ea1346ac8f4e3
Tom Marien <tommarien@gmail.com>
2025-02-26 19:27:49 +0100

    nvim: move onedark highlights overrides into autocmd

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd('ColorScheme', {
+    pattern = 'onedark',
+    callback = function()
+        vim.api.nvim_set_hl(0, 'QuickFixLine', { link = 'CursorLine' })
+    end,
+})
+
 return {
     {
         'navarasu/onedark.nvim',
@@ -12,7 +19,6 @@ return {
         config = function(_, opts)
             require('onedark').setup(opts)
             vim.cmd('colorscheme onedark')
-            vim.api.nvim_set_hl(0, 'QuickFixLine', { link = 'CursorLine' })
         end,
     },
 }
