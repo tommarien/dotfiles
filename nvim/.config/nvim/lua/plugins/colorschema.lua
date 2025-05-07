61edb3dd1dcfc36d71ad6b1490b6d237fd34162a
Tom Marien <tommarien@gmail.com>
2025-05-07 20:29:03 +0200

    nvim: remove jellybeans

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -5,13 +5,6 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     end,
 })
 
-vim.api.nvim_create_autocmd('ColorScheme', {
-    pattern = 'jellybeans',
-    callback = function()
-        vim.cmd('highlight Comment gui=italic')
-    end,
-})
-
 return {
     {
         'savq/melange-nvim',
@@ -21,23 +14,4 @@ return {
             vim.cmd('colorscheme melange')
         end
     },
-    {
-        'wtfox/jellybeans.nvim',
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        opts = {
-            flat_ui = false,
-            italics = false,
-            transparent = true,
-            on_colors = function(c)
-                if vim.o.background == 'dark' then
-                    c.background = '#1f2329'
-                end
-            end
-        },
-        init = function()
-            vim.cmd('colorscheme jellybeans')
-        end
-    }
 }
