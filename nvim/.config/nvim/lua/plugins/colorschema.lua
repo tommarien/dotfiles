0a8e248b8bed3f7f7c7795e6d2a2bcc37aa32794
Tom Marien <tommarien@gmail.com>
2025-06-15 16:13:11 +0200

    neovim: updated plugins

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -13,7 +13,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     pattern = 'sonokai',
     callback = function()
         vim.cmd('highlight CursorLine guibg=none')
-        vim.cmd('highlight SnacksIndentScope guifg=#5d5299')
+        vim.cmd('highlight SnacksIndentScope guifg=#6C64A0')
         vim.cmd('highlight SnacksPickerCursorLine guibg=#394634')
         vim.cmd('highlight SnacksPickerListCursorLine guibg=#394634')
         vim.api.nvim_set_hl(0, 'PackageInfoUpTodateVersion', { link = 'Added' })
