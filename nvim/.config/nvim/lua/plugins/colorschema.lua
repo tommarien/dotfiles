0f1e8e24ab406ecbc48548b2dcb89ad17a2825f9
Tom Marien <tommarien@gmail.com>
2025-06-21 10:09:16 +0200

    config: added rose-pine-moon and swithed back to melange

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -2,7 +2,6 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     pattern = 'melange',
     callback = function()
         vim.cmd('highlight CursorLine guibg=none')
-        vim.cmd('highlight Normal guibg=none')
         vim.cmd('highlight SnacksIndent guifg=#3a3734')
         vim.cmd('highlight SnacksIndentScope guifg=#8a7d73')
         vim.cmd('highlight String gui=none')
@@ -21,12 +20,22 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     end,
 })
 
+vim.api.nvim_create_autocmd('ColorScheme', {
+    pattern = 'rose-pine-moon',
+    callback = function()
+        vim.cmd('highlight CursorLine guibg=none')
+        vim.cmd('highlight Comment gui=italic')
+    end,
+})
 
 return {
     {
         'savq/melange-nvim',
         priority = 1000,
         lazy = false,
+        init = function()
+            vim.cmd('colorscheme melange')
+        end
     },
     {
         'sainnhe/sonokai',
@@ -36,7 +45,18 @@ return {
             vim.g.sonokai_better_performance = 1
             vim.g.sonokai_style = 'shusia'
             vim.g.sonokai_transparent_background = 1
-            vim.cmd('colorscheme sonokai')
         end
+    },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        priority = 1000,
+        lazy = false,
+        opts = {
+            styles = {
+                italic = false,
+                transparency = true,
+            },
+        },
     }
 }
