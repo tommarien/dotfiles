5da5f9448b39d9c09aaff9ddb7957fed57b9fefc
Tom Marien <tommarien@gmail.com>
2025-06-29 20:21:56 +0200

    config: tweak ghostty
    - updated nvim plugins

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -3,6 +3,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     callback = function()
         vim.cmd('highlight CursorLine guibg=none')
         vim.cmd('highlight LspReferenceText gui=none')
+        vim.cmd('highlight NormalFloat guibg=none')
         vim.cmd('highlight SnacksIndent guifg=#3a3734')
         vim.cmd('highlight SnacksIndentScope guifg=#8a7d73')
         vim.cmd('highlight String gui=none')
