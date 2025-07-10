43458aa353bfa1a8ac1f98348c3781654ecb49c3
Tom Marien <tommarien@gmail.com>
2025-07-10 10:17:27 +0200

    nvim: colorscheme tweaks

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -3,7 +3,9 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     callback = function()
         vim.cmd('highlight Comment guifg=#9d8b78')
         vim.cmd('highlight CursorLine guibg=none')
+        vim.cmd('highlight GitSignsCurrentLineBlame guifg=#7d6b58')
         vim.cmd('highlight LspReferenceText guibg=none gui=none')
+        vim.cmd('highlight MatchParen guibg=#413a30')
         vim.cmd('highlight NormalFloat guibg=none')
         vim.cmd('highlight String gui=none')
     end,
