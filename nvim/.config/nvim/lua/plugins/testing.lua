9e79de7ad08fe9d83415d9c347b368bfcc195248
Tom Marien <tommarien@gmail.com>
2025-08-03 11:23:31 +0200

    nvim(vimtest): added select jsrunner script

--- nvim/.config/nvim/lua/plugins/testing.lua
+++ nvim/.config/nvim/lua/plugins/testing.lua
@@ -1,3 +1,16 @@
+local function set_js_test_runner()
+    vim.ui.input(
+        { prompt = 'Enter JS test runner (e.g., jest): ' },
+        function(input)
+            if input and input ~= '' then
+                vim.g['test#javascript#runner'] = input
+                vim.notify('Set JS test runner to: ' .. input, vim.log.levels.INFO)
+            else
+                vim.notify('No runner set.', vim.log.levels.WARN)
+            end
+        end)
+end
+
 return {
     {
         'vim-test/vim-test',
@@ -6,12 +19,12 @@ return {
             'preservim/vimux'
         },
         keys = {
-            { "<leader>tt", vim.cmd.TestNearest,     desc = 'Run Nearest Test' },
-            { "<leader>tf", vim.cmd.TestFile,        desc = 'Run Test File' },
-            { "<leader>ts", vim.cmd.TestSuite,       desc = 'Run All Test Files' },
-            { "<leader>tr", vim.cmd.TestLast,        desc = 'Run Last Test Run' },
-            { "<leader>tm", vim.cmd.VimuxZoomRunner, desc = 'Maximize Test Runner' }
-
+            { '<leader>tt', vim.cmd.TestNearest,     desc = 'Run Nearest Test' },
+            { '<leader>tf', vim.cmd.TestFile,        desc = 'Run Test File' },
+            { '<leader>ts', vim.cmd.TestSuite,       desc = 'Run All Test Files' },
+            { '<leader>tr', vim.cmd.TestLast,        desc = 'Run Last Test Run' },
+            { '<leader>tm', vim.cmd.VimuxZoomRunner, desc = 'Maximize Test Runner' },
+            { '<leader>tj', set_js_test_runner,      desc = 'Set JS Test Runner' }
         },
         config = function()
             vim.g['test#strategy'] = 'vimux'
