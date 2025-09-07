7b26ed195636a7709135b41ecb28ca9c8d87cd4c
Tom Marien <tommarien@gmail.com>
2025-09-07 14:06:32 +0200

    theme: welcome tokyonight-night

--- nvim/.config/nvim/lua/plugins/colorschema.lua
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -1,41 +1,19 @@
 return {
     {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        config = function()
-            require('rose-pine').setup({
-                variant = 'auto',      -- auto, main, moon, or dawn
-                dark_variant = 'moon', -- main, moon, or dawn
-                dim_inactive_windows = false,
-                extend_background_behind_borders = true,
-
-                enable = {
-                    terminal = true,
-                    legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
-                    migrations = true,         -- Handle deprecated options automatically
-                },
-
-                styles = {
-                    bold = true,
-                    italic = false,
-                    transparency = true,
-                },
-
-                palette = {
-                    --Override the builtin palette per variant
-                    moon = {
-                        base = '#1c1c1e',
-                    },
-                },
-
-                -- NOTE: Highlight groups are extended (merged) by default. Disable this
-                -- per group via `inherit = false`
-                highlight_groups = {
-                    Comment = { italic = true },
-                    DiffviewNormal = { bg = 'NONE' }
-                },
-            })
-            vim.cmd('colorscheme rose-pine')
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true,
+            styles = {
+                keywords = { italic = false },
+                -- Background styles. Can be 'dark', 'transparent' or 'normal'
+                sidebars = 'normal', -- style for sidebars, see below
+                floats = 'normal',   -- style for floating windows
+            }
+        },
+        init = function()
+            vim.cmd('colorscheme tokyonight-night')
         end
-    },
+    }
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -47,7 +47,7 @@ export CPPFLAGS="-I/opt/homebrew/opt/openssl@3/include"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig"
 
 # Load fzf theme
-source $HOME/.zsh/fzf-rose-pine-moon-theme
+source $HOME/.zsh/fzf-tokyonight-night-theme
 
 # Add custom aliases
 source $HOME/.zsh/aliases
