0a0aa1a01800f57fe30fb23740ee21ab42dbbfcc
Tom Marien <tommarien@gmail.com>
2025-09-20 13:25:28 +0200

    some tweaks

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -353,4 +353,22 @@ return {
             require('refactoring').setup {}
         end,
     },
+    {
+        'ggandor/leap.nvim',
+        config = function()
+            local leap = require('leap')
+            -- Exclude whitespace and the middle of alphabetic words from preview:
+            --   foobar[baaz] = quux
+            --   ^----^^^--^^-^-^--^
+            leap.opts.preview_filter =
+                function(ch0, ch1, ch2)
+                    return not (
+                        ch1:match('%s') or
+                        ch0:match('%a') and ch1:match('%a') and ch2:match('%a')
+                    )
+                end
+
+            leap.set_default_mappings()
+        end
+    }
 }
