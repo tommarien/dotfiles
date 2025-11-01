2c25e9ed0a27fc821211bdc8f6eaa1a26971c7d0
Tom Marien <tommarien@gmail.com>
2025-11-01 12:53:16 +0100

    nvim: more colorscheme tweaks

--- nvim/.config/nvim/lua/github_dark_plus/utils/lualine.lua
+++ nvim/.config/nvim/lua/github_dark_plus/utils/lualine.lua
@@ -3,39 +3,39 @@ return function()
     local theme = {}
 
     theme.normal = {
-        a = { bg = c.blue, fg = c.bg, gui = "bold" },
+        a = { bg = c.blue, fg = c.bg, gui = 'bold' },
         b = { bg = c.bg_darker, fg = c.fg },
         c = { bg = c.bg_darker, fg = c.fg_dim },
     }
 
     theme.insert = {
-        a = { bg = c.green_light, fg = c.bg, gui = "bold" },
+        a = { bg = c.green_light, fg = c.bg, gui = 'bold' },
         b = { bg = c.bg_darker, fg = c.fg },
     }
 
     theme.terminal = {
-        a = { bg = c.blue, fg = c.bg, gui = "bold" },
+        a = { bg = c.blue, fg = c.bg, gui = 'bold' },
         b = { bg = c.bg_darker, fg = c.fg },
     }
 
     theme.command = {
-        a = { bg = c.yellow, fg = c.bg, gui = "bold" },
+        a = { bg = c.yellow, fg = c.bg, gui = 'bold' },
         b = { bg = c.bg_darker, fg = c.fg },
     }
 
     theme.visual = {
-        a = { bg = c.purple_light, fg = c.bg, gui = "bold" },
+        a = { bg = c.purple_light, fg = c.bg, gui = 'bold' },
         b = { bg = c.bg_darker, fg = c.fg },
     }
 
     theme.replace = {
-        a = { bg = c.cyan, fg = c.bg, gui = "bold" },
+        a = { bg = c.cyan, fg = c.bg, gui = 'bold' },
         b = { bg = c.bg_darker, fg = c.fg },
     }
 
     theme.inactive = {
         a = { bg = c.bg, fg = c.blue },
-        b = { bg = c.bg, fg = c.fg, gui = "bold" },
+        b = { bg = c.bg, fg = c.fg, gui = 'bold' },
         c = { bg = c.bf, fg = c.fg },
     }
 
