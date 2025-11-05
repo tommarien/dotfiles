bdfe4032cb47e9f470db9834d04a5aa3d255db55
Tom Marien <tommarien@gmail.com>
2025-11-05 11:50:37 +0100

    nvim(cmp): richer completion

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -48,18 +48,14 @@ return {
     },
     {
         'saghen/blink.cmp',
-        -- optional: provides snippets for the snippet source
-        dependencies = { 'rafamadriz/friendly-snippets' },
+        dependencies = {
+            'rafamadriz/friendly-snippets',
+            'xzbdmw/colorful-menu.nvim',
+        },
 
         enabled = not vim.g.vscode,
 
-        -- use a release tag to download pre-built binaries
         version = '1.*',
-        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        -- If you use nix, you can build from source using latest nightly rust with:
-        -- build = 'nix run .#build-plugin',
-
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
@@ -108,7 +104,26 @@ return {
             },
 
             -- (Default) Only show the documentation popup when manually triggered
-            completion = { documentation = { auto_show = false } },
+            completion = {
+                documentation = { auto_show = false },
+                menu = {
+                    draw = {
+                        -- We don't need label_description now because label and label_description are already
+                        -- combined together in label by colorful-menu.nvim.
+                        columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+                        components = {
+                            label = {
+                                text = function(ctx)
+                                    return require('colorful-menu').blink_components_text(ctx)
+                                end,
+                                highlight = function(ctx)
+                                    return require('colorful-menu').blink_components_highlight(ctx)
+                                end,
+                            },
+                        },
+                    },
+                },
+            },
 
             -- Default list of enabled providers defined so that you can extend it
             -- elsewhere in your config, without redefining it, due to `opts_extend`
