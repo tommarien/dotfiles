c654c2a0ca21c205681673b6a10ba1a9aec7900e
Tom Marien <tommarien@gmail.com>
2025-11-30 14:57:48 +0100

    nvim: add todo comments plugin and update themes

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -29,6 +29,15 @@ return {
         end
     },
     {
+        'folke/todo-comments.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    {
         'brenoprata10/nvim-highlight-colors',
         event = 'VeryLazy',
         enabled = not vim.g.vscode,
