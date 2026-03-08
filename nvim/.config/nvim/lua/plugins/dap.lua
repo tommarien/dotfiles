d9a99522186360764dbe708a0b9bd3fda7317b40
Tom Marien <tommarien@gmail.com>
2026-03-08 11:03:47 +0100

    nvim: updated plugins and replace nvim-dap-ui with nvim-dap-view

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -22,7 +22,6 @@ return {
                 "jay-babu/mason-nvim-dap.nvim",
                 dependencies = "mason.nvim",
             },
-            "rcarriga/nvim-dap-ui",
             -- virtual text for the debugger
             {
                 "theHamsta/nvim-dap-virtual-text",
@@ -53,7 +52,6 @@ return {
             { "<leader>dr", function() require("dap").repl.toggle() end,                                          desc = "Toggle REPL" },
             { "<leader>ds", function() require("dap").session() end,                                              desc = "Session" },
             { "<leader>dt", function() require("dap").terminate() end,                                            desc = "Terminate" },
-            { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
         },
 
         config = function()
@@ -108,37 +106,40 @@ return {
             vscode.json_decode = function(str)
                 return vim.json.decode(json.json_strip_comments(str))
             end
-
-            -- Extends dap.configurations with entries read from .vscode/launch.json
-            if vim.fn.filereadable(".vscode/launch.json") then
-                vscode.load_launchjs()
-            end
         end,
+    },
+    {
+        "igorlfs/nvim-dap-view",
+        -- let the plugin lazy load itself
+        lazy = false,
+        ---@module 'dap-view'
+        ---@type dapview.Config
+        opts = {},
     },
 
     -- fancy UI for the debugger
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "nvim-neotest/nvim-nio" },
-        -- stylua: ignore
-        keys = {
-            { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
-            { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
-        },
-        opts = {},
-        config = function(_, opts)
-            local dap = require("dap")
-            local dapui = require("dapui")
-            dapui.setup(opts)
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open({})
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close({})
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close({})
-            end
-        end,
-    },
+    -- {
+    --     "rcarriga/nvim-dap-ui",
+    --     dependencies = { "nvim-neotest/nvim-nio" },
+    --     -- stylua: ignore
+    --     keys = {
+    --         { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+    --         { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
+    --     },
+    --     opts = {},
+    --     config = function(_, opts)
+    --         local dap = require("dap")
+    --         local dapui = require("dapui")
+    --         dapui.setup(opts)
+    --         dap.listeners.after.event_initialized["dapui_config"] = function()
+    --             dapui.open({})
+    --         end
+    --         dap.listeners.before.event_terminated["dapui_config"] = function()
+    --             dapui.close({})
+    --         end
+    --         dap.listeners.before.event_exited["dapui_config"] = function()
+    --             dapui.close({})
+    --         end
+    --     end,
+    -- },
 }
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -375,33 +375,4 @@ return {
             telescope.load_extension('fzf');
         end
     },
-    {
-        url = "https://codeberg.org/andyg/leap.nvim",
-        config = function()
-            local leap = require('leap')
-            -- Highly recommended: define a preview filter to reduce visual noise
-            -- and the blinking effect after the first keypress
-            -- (`:h leap.opts.preview`). You can still target any visible
-            -- positions if needed, but you can define what is considered an
-            -- exceptional case.
-            -- Exclude whitespace and the middle of alphabetic words from preview:
-            --   foobar[baaz] = quux
-            --   ^----^^^--^^-^-^--^
-            leap.opts.preview = function(ch0, ch1, ch2)
-                return not (
-                    ch1:match('%s')
-                    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
-                )
-            end
-
-            -- Define equivalence classes for brackets and quotes, in addition to
-            -- the default whitespace group:
-            leap.opts.equivalence_classes = {
-                ' \t\r\n', '([{', ')]}', '\'"`'
-            }
-
-            vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
-            vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
-        end
-    }
 }
