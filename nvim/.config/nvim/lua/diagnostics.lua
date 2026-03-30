1e3d8f7e35d5f02fae1d4889b92d98065705f32c
Tom Marien <tommarien@gmail.com>
2026-03-30 20:09:11 +0200

    nvim(diag): diagnostics prev next -> jump

--- nvim/.config/nvim/lua/diagnostics.lua
+++ nvim/.config/nvim/lua/diagnostics.lua
@@ -3,10 +3,10 @@ local utils = require('utils');
 
 utils.map('n', '<leader>e', vim.diagnostic.open_float, { silent = true, desc = 'Show diagnostics' })
 utils.map('n', '[d', function()
-    vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.WARN } })
+    vim.diagnostic.jump({ count = -1, severity = { min = vim.diagnostic.severity.WARN } })
 end, { silent = true, desc = 'Previous diagnostic' })
 utils.map('n', ']d', function()
-    vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.WARN } })
+    vim.diagnostic.jump({ count = 1, severity = { min = vim.diagnostic.severity.WARN } })
 end, { silent = true, desc = 'Next diagnostic' })
 utils.map('n', '<leader>q', vim.diagnostic.setloclist, { silent = true, desc = 'Show diagnostics quickfix' })
 
