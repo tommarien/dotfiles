8f21ede644b83b606046da311fc016c6eb7a7f6d
Tom Marien <tommarien@gmail.com>
2026-04-23 20:00:36 +0200

    nvim: switch to agentic.nvim and update plugins

---
+++ nvim/.config/nvim/lua/plugins/agentic.lua
@@ -0,0 +1,60 @@
+return {
+    {
+        'carlos-algms/agentic.nvim',
+
+        --- @type agentic.PartialUserConfig
+        opts = {
+            -- Any ACP-compatible provider works. Built-in: 'claude-agent-acp' | 'gemini-acp' | 'codex-acp' | 'opencode-acp' | 'cursor-acp' | 'copilot-acp' | 'auggie-acp' | 'mistral-vibe-acp' | 'cline-acp' | 'goose-acp'
+            provider = 'claude-agent-acp', -- setting the name here is all you need to get started
+            windows = {
+                width = '30%',
+            }
+        },
+
+        -- these are just suggested keymaps; customize as desired
+        keys = {
+            {
+                '<leader>aa',
+                function() require('agentic').toggle() end,
+                mode = { 'n' },
+                desc = 'Toggle Agentic Chat'
+            },
+            {
+                '<leader>af',
+                function() require('agentic').add_selection_or_file_to_context() end,
+                mode = { 'n', 'v' },
+                desc = 'Add file or selection to Agentic to Context'
+            },
+            {
+                '<leader>as',
+                function() require('agentic').new_session() end,
+                mode = { 'n' },
+                desc = 'New Agentic Session'
+            },
+            {
+                '<leader>ar', -- ai Restore
+                function()
+                    require('agentic').restore_session()
+                end,
+                mode = { 'n' },
+                desc = 'Agentic Restore session',
+                silent = true,
+            },
+            {
+                '<leader>ad', -- ai Diagnostics
+                function()
+                    require('agentic').add_current_line_diagnostics()
+                end,
+                desc = 'Add current line diagnostic to Agentic',
+                mode = { 'n' },
+            },
+            {
+                '<leader>aD', -- ai all Diagnostics
+                function()
+                    require('agentic').add_buffer_diagnostics()
+                end,
+                desc = 'Add all buffer diagnostics to Agentic',
+                mode = { 'n' },
+            },
+        },
+    } }
