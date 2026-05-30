c916ff04f6375647f259d11b87ebb5ca67ad7e9d
Tom Marien <tommarien@gmail.com>
2026-05-30 19:29:30 +0200

    nvim: updated plugins and disable copilot by default

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -7,7 +7,7 @@ return {
         config = function()
             require('copilot').setup({
                 suggestion = {
-                    enabled = true,
+                    enabled = false,
                     auto_trigger = true,
                     keymap = {
                         accept = false,
