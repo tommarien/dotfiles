33b6b4e2f213f23feb4a616e5229127ab71b118f
Tom Marien <tommarien@gmail.com>
2026-08-03 08:33:19 +0200

    nvim: added flash, see if can replace eyeliner with extras

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -121,15 +121,15 @@ return {
             }
         end
     },
-    {
-        'jinh0/eyeliner.nvim',
-        event = 'VeryLazy',
-        enabled = not vim.g.vscode,
-        opts = {
-            highlight_on_key = true,
-            dim = true,
-        }
-    },
+    -- {
+    --     'jinh0/eyeliner.nvim',
+    --     event = 'VeryLazy',
+    --     enabled = not vim.g.vscode,
+    --     opts = {
+    --         highlight_on_key = true,
+    --         dim = true,
+    --     }
+    -- },
     {
         'stevearc/conform.nvim',
         event = 'VeryLazy',
@@ -359,6 +359,18 @@ return {
                 '<cmd>Trouble qflist toggle<cr>',
                 desc = 'Quickfix List (Trouble)',
             },
+        },
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+            { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+            -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
     }
 }
